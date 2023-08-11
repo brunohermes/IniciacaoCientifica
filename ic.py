02546f152160a0e73d1d35d834d05e3a21a7d3a7
Bruno Hermes <48295386+brunohermes@users.noreply.github.com>
2023-08-10 23:48:05 -0300

    Update ic.py

--- ic.py
+++ ic.py
@@ -1,16 +1,11 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Jul 29 20:57:34 2023
 
 @author: Bruno
 """
-
 import pandas as pd
 #import numpy as np
 
 source = pd.read_excel("dados.xlsx")
 
-source = source.drop(columns=['nome_4md','regiao', 'atbt', 'qtde_u_csrecebem_os_creditos', 'n_sistemas'])
-
-source2 = source[data.uf.str.contains("PR", na=false)]
-
+source = source.drop(columns=['nome_4md','regiao', 'atbt', 'qtde_u_csrecebem_os_creditos', 'n_sistemas']) [source["uf"].str.contains("PR")]
